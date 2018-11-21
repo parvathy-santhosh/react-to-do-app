c456b30e67e1d786b8caa186b98f45b7e32795a3
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-21 23:38:26 +0530

    Updates

---
+++ precache-manifest.6a67f7fb65e0d4dae8536812c085c004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11c71984401016cb3f1",
+    "url": "/react-to-do-app/static/js/runtime~main.d11c7198.js"
+  },
+  {
+    "revision": "94661a99c9460bb1eb99",
+    "url": "/react-to-do-app/static/js/main.94661a99.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/react-to-do-app/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "94661a99c9460bb1eb99",
+    "url": "/react-to-do-app/static/css/main.f60e173a.chunk.css"
+  },
+  {
+    "revision": "28b1ebb67fc8faaa118060ba3f3d2b90",
+    "url": "/react-to-do-app/index.html"
+  }
+];
